9a9ad753b9a27c36a0a4e0f1ee5707dc3ac608b8
milena <milenasantos@bemol.com.br>
2021-11-10 17:18:58 -0400

    criando o aluno

--- app/src/main/java/com/milena/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/milena/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package com.milena.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.milena.agenda.R;
+import com.milena.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -13,5 +18,29 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
         setTitle("Formulário de Alunos"); //Título da AppBar
+
+        //Pego os id
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        //Pego os id da button
+        Button butaoSalvar = findViewById(R.id.activity_fomulario_aluno_button_salvar_aluno);
+
+        butaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone =  campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+                    
+                //testando
+                Toast.makeText(FormularioAlunoActivity.this, "Aluno criado:"+alunoCriado.getNome(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
